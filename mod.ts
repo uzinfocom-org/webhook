365f150b4a8eb657eab4e021b85ad321f08b3f93
UwUssimo Robinson <uwussimo@icloud.com>
2022-09-06 14:36:31 +0500

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -11,7 +11,7 @@ const webhook = async () => {
     if (req.method == "POST") {
       switch (url.pathname) {
         case "/github":
-          console.log(req.body);
+          console.log(req.json());
           return new Response("Github Webhook");
         case "/bot":
           return await handle(req);
