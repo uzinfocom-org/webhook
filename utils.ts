5576f112f96856b8826f93a0d96e9cce05c6c8a7
UwUssimo Robinson <uwussimo@icloud.com>
2022-09-06 14:44:56 +0500

    Update utils.ts

--- utils.ts
+++ utils.ts
@@ -1,14 +1,14 @@
 // Find out whether what kind of github webhook event it is
+// required states: push, pull_request, issues, issue_comment
 export const stateFinder = (data: any): string => {
-  const { action, pull_request, issue, comment } = data;
-  if (action === "opened" && pull_request) {
-    return "pull_request_opened";
-  } else if (action === "closed" && pull_request) {
-    return "pull_request_closed";
-  } else if (action === "created" && issue) {
-    return "issue_created";
-  } else if (action === "created" && comment) {
-    return "comment_created";
+  if (data.issue) {
+    return "issues";
+  } else if (data.pull_request) {
+    return "pull_request";
+  } else if (data.comment) {
+    return "issue_comment";
+  } else if (data.ref) {
+    return "push";
   } else {
     return "unknown";
   }
