b0072551192165b30a159ca206934118a8fcdb3b
UwUssimo Robinson <uwussimo@icloud.com>
2022-09-06 17:07:13 +0500

    [fix] customizing push messages

--- mod.ts
+++ mod.ts
@@ -1,15 +1,28 @@
 /// <reference lib="dom" />
 /// <reference lib="dom.iterable" />
-import { Bot, on, webhooks } from "./deps.ts";
+import { Bot, InlineKeyboard, on, webhooks } from "./deps.ts";
 
 const env = Deno.env.toObject();
 const channel = env["CHANNEL"];
 const bot = new Bot(env["TOKEN"] || "");
 
+async function push(message: string, link = "") {
+  switch (link) {
+    case "":
+      return await bot.api.sendMessage(channel, message, {
+        parse_mode: "HTML",
+      });
+    default:
+      return await bot.api.sendMessage(channel, message, {
+        parse_mode: "HTML",
+        reply_markup: new InlineKeyboard().url("View it on GitHub", link),
+      });
+  }
+}
+
 webhooks()(
-  // ping
   on("ping", async () => {
-    await bot.api.sendMessage(channel, "GitHub Ping from Ping Webhook");
+    await push("GitHub Ping from Webhook");
   }),
   on("push", async (event) => {
     const { ref, commits, repository } = event;
@@ -17,21 +30,24 @@ webhooks()(
     const { name } = repository;
     const commit = commits[0];
     const { message, url } = commit;
-    const text = `Pushed to ${branch} at ${name} - ${message} ${url}`;
-    await bot.api.sendMessage(channel, text);
+    const text = `📦 <b>Pushed to ${branch} at ${name}</b>\n\n${message}`;
+    await push(text, url);
   }),
   on("issues", async ({ issue }, _context) => {
-    const text = `Issue #${issue.number} was ${issue.body}`;
-    await bot.api.sendMessage(channel, text).then();
+    await push(
+      `⚠️ <b>New issue:</b> ${issue.title}\n\n${issue.body}`,
+      issue.html_url,
+    );
   }),
   on("issue_comment", async ({ issue, comment }, _context) => {
-    const text =
-      `@${comment.user.login} commented on issue #${issue.number}: ${comment.body}`;
-    await bot.api.sendMessage(channel, text);
+    await push(
+      `📢 <b>New comment on issue:</b>\n\n<u>${issue.title}</u>\n<a href="${comment.user.html_url}">@${comment.user.login}</a>: ${comment.body}`,
+      comment.html_url,
+    );
   }),
-  // deployment
   on("deployment", async ({ deployment }, _context) => {
-    const text = `Deployment #${deployment.id} was ${deployment.environment}`;
-    await bot.api.sendMessage(channel, text);
+    await push(
+      `🎛 <b>${deployment.environment}</b> deployment with <b>#${deployment.id}</b> has been deployed!`,
+    );
   }),
 );
